c02da13f7af191873d995e590fc557c63eefc6b2
Santiago Grangetto <santiago.grangetto@southworks.com>
2021-02-05 10:02:06 -0300

    Add copyright header

--- tools/devops-tasks/GitHubPRComment/githubComment/index.ts
+++ tools/devops-tasks/GitHubPRComment/githubComment/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import taskLibrary = require('azure-pipelines-task-lib/task');
 import gitClient = require('@octokit/rest');
 import path = require('path');
